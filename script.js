607c9d89a06fdd690713f50dec5dd4b06fa9bb0e
SALIM <solonkasalim13@gmail.com>
2023-02-21 15:05:14 +0300

    Second Commit

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ window.addEventListener("scroll",() =>{
 	header.classList.toggle("sticky",window.scrollY > 0);
 
 })
+	const navigation = document.querySelector('.navigation');
 function toggleMenu(){
 	const menuToggle=document.querySelector(".menu-toggle");
 	menuToggle.classList.toggle('active');
-	const navigation = document.querySelector('.navigation');
 	navigation.classList.toggle("active");
-	
+   let links=document.querySelectorAll("li")
+		links.forEach(link =>link.addEventListener("click",function(){
+		navigation.classList.remove("active");
+		menuToggle.classList.remove("active");
+	}));
+
 }
+
